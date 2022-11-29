e167d308c97eb1806f968131a2fb7eefafb661db
Krzysztof Gruca <kgruca21@gmail.com>
2022-11-29 00:12:16 -0800

    started to configure auth with firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBrpdFXuN6YG8Id6tq34Z3UVknfPTAJ1Bk",
+    authDomain: "fxy-clothing-db.firebaseapp.com",
+    projectId: "fxy-clothing-db",
+    storageBucket: "fxy-clothing-db.appspot.com",
+    messagingSenderId: "1021214533129",
+    appId: "1:1021214533129:web:fad24246224c6f932e3ad8"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
