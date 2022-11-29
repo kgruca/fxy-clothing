b3acc647e1794c876e5a507582a72136a93af4e4
Krzysztof Gruca <kgruca21@gmail.com>
2022-11-28 23:31:01 -0800

    sign in page template, added sign in link to App.js and navigation.component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -14,6 +14,9 @@ const Navigation = () => {
                     <Link className="nav-link" to='/shop'>
                         SHOP
                     </Link>
+                    <Link className="signIn" to='/SignIn'>
+                        SIGN IN
+                    </Link>
                 </div>
             </div>
             <Outlet />
