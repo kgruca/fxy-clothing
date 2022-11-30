cf4831a3ce1df6c03053b514787f4f2ab4d7b11c
Krzysztof Gruca <kgruca21@gmail.com>
2022-11-29 22:54:52 -0800

    setting up user documents with Firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import {
     GoogleAuthProvider 
 } from 'firebase/auth';
 
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyBrpdFXuN6YG8Id6tq34Z3UVknfPTAJ1Bk",
     authDomain: "fxy-clothing-db.firebaseapp.com",
@@ -13,15 +20,27 @@ const firebaseConfig = {
     storageBucket: "fxy-clothing-db.appspot.com",
     messagingSenderId: "1021214533129",
     appId: "1:1021214533129:web:fad24246224c6f932e3ad8"
-  };
+};
   
-  // Initialize Firebase
-  const firebaseApp = initializeApp(firebaseConfig);
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
-    prompt: "select_account"
-  });
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+prompt: "select_account"
+});
 
-  export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    console.log(userSnapshot);
+};
