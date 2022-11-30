68e0a400f6961986e682aad6ba8d4f41588d6455
Krzysztof Gruca <kgruca21@gmail.com>
2022-11-30 00:49:51 -0800

    added sign in component, firebase utils, and brought in firebase libraries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -37,10 +37,24 @@ export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
-
     console.log(userDocRef);
 
     const userSnapshot = await getDoc(userDocRef);
 
-    console.log(userSnapshot);
+    if (!userSnapshot.exists()) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('Error creating the user', error.message);
+        } 
+    }
+
+    return userDocRef;
 };
